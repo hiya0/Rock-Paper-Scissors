10017355ded8d3cb20f4b187a3d6900dcf8a6dea
Hiya Devli <devlihiya9@gmail.com>
2021-06-10 17:30:12 +0530

    structure defined

--- app.js
+++ app.js
@@ -1,95 +1,24 @@
-let userScore=0;
-let compScore=0;
-const userScore_span=document.getElementById('user-score');
-const compScore_span=document.getElementById('comp-score');
-const scoreBoard_div= document.querySelector(".score-board");
-const result_p = document.querySelector(".result");
-const rock_div = document.getElementById("r");
-const paper_div = document.getElementById("p");
-const scissors_div = document.getElementById("s");
+const express= require("express");
+var app = express();
+const path = require('path');
+const router = express.Router();
 
-const smallUser="user".fontsize(3).sub();
-const smallComp="comp".fontsize(3).sub();
+app.use(express.static(__dirname + '/public'));
 
+router.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/views/index.html'));
+});
 
-function getComputerChoice(){
-	const choices = ['r','p','s'];
-	const randomNumber=Math.floor(Math.random()*3);
-	return choices[randomNumber];
-}
+router.get('/computer',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/views/computer/comp.html'));
+});
 
-function convert(letter){
-	if(letter=='r') return "Rock";
-	if(letter=='p') return "Paper";
-	if(letter=='s') return "Scissors";
-}
+router.get('/multiplayer',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/views/multiplayer/room.html'));
+});
 
-function win(userChoice,computerChoice){
-	const userChoice_div=document.getElementById(userChoice);
-	userScore++;
-	userScore_span.innerHTML=userScore;
-	compScore_span.innerHTML=compScore;
-	result_p.innerHTML=`${convert(userChoice)}${smallUser} beats ${convert(computerChoice)}${smallComp}. You win! 🔥 🔥`;
-	document.getElementById(userChoice).classList.add('green-glow');
-	setTimeout(()=>userChoice_div.classList.remove('green-glow'),300);
-}
+app.use('/', router);
+app.listen(process.env.port || 3000);
 
-function lose(userChoice,computerChoice){
-	const userChoice_div=document.getElementById(userChoice);
-	compScore++;
-	userScore_span.innerHTML=userScore;
-	compScore_span.innerHTML=compScore;
-	result_p.innerHTML=`${convert(computerChoice)}${smallComp} beats ${convert(userChoice)}${smallUser}. You lose! 💩 💩 💩 💩`;
-	document.getElementById(userChoice).classList.add('red-glow');
-	setTimeout(()=>userChoice_div.classList.remove('red-glow'),300);
-}
-
-function draw(userChoice,computerChoice){
-	const userChoice_div=document.getElementById(userChoice);
-	userScore_span.innerHTML=userScore;
-	compScore_span.innerHTML=compScore;
-	result_p.innerHTML=`${convert(computerChoice)}${smallComp} equals ${convert(userChoice)}${smallUser}. It's a draw! 🤝`;
-	document.getElementById(userChoice).classList.add('gray-glow');
-	setTimeout(()=>userChoice_div.classList.remove('gray-glow'),300);
-}
-
-
-function game(userChoice){
-	const computerChoice=getComputerChoice();
-		switch(userChoice + computerChoice){
-		case "pr" :
-		case "sp" :
-		case "rs" :
-			win(userChoice,computerChoice);
-			break;
-		case "rp" :
-		case "ps" :
-		case "sr" :
-			lose(userChoice,computerChoice);
-			break;
-		case "rr" :
-		case "pp" :
-		case "ss" :
-			draw(userChoice,computerChoice);
-			break;
-	}
-}
-
-
-
-function main(){
-	rock_div.addEventListener('click', ()=> game("r"));
-	paper_div.addEventListener('click', ()=> game("p"));
-	scissors_div.addEventListener('click', ()=> game("s"));
-}
-main();
-
-
-
-
-
-
-
-
-
+console.log('Running at Port 3000');
 
